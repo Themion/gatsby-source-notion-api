b3be75fdf0404b9abaca9cbf2cc3616bb6be1978
Themion <themion@naver.com>
2024-06-30 07:19:26 +0900

    fix: remove redundant node-fetch

--- src/notion-api/get-pages.js
+++ src/notion-api/get-pages.js
@@ -1,4 +1,3 @@
-const fetch = require('node-fetch');
 const { errorMessage } = require('../error-message');
 const { getBlocks } = require('./get-blocks');
 const { Client } = require('@notionhq/client');
