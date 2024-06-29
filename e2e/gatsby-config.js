073c1c286f9cc35e7bfdf198f6d59bf72de072f4
Themion <themion@naver.com>
2024-06-29 19:11:32 +0900

    feat: create project for test (#3)

---
+++ e2e/gatsby-config.js
@@ -0,0 +1,57 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
+ */
+
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Default Starter`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
+    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+  },
+  plugins: [
+    `gatsby-plugin-image`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `@themion/gatsby-source-notion-api`,
+      options: {
+        token: "secret_Kfd4ZfFk7kjmy7xMb2GLKKWVGtkoQnNoyWlaxeQT40L",
+        databaseId: "59a089511d7048128c2b2882a7127434",
+      },
+    },
+    {
+      resolve: `@themion/gatsby-source-notion-api`,
+      options: {
+        token: "secret_Kfd4ZfFk7kjmy7xMb2GLKKWVGtkoQnNoyWlaxeQT40L",
+        databaseId: "10e318d19f28432abb8fd38e9397c06f",
+      },
+    },
+  ],
+}
