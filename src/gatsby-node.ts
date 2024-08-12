f93b4d388ba1837ccf3a8e1f0f15fbd927c6e5d2
Themion <themion@naver.com>
2024-08-12 13:33:35 +0000

    fix: remove logging

--- src/gatsby-node.ts
+++ src/gatsby-node.ts
@@ -1,22 +1,9 @@
-import type { GatsbyNode, NodePluginArgs, PluginOptions, Reporter } from 'gatsby';
+import type { GatsbyNode, PluginOptions } from 'gatsby';
 import { importNotionSource } from '~/import-notion-source';
 import type { Options } from '~/types';
 
-const time = async (args: NodePluginArgs, options: PluginOptions & Options, message: string) => {
-  const { reporter } = args;
-  const activity = reporter.activityTimer(message);
-  activity.start()
-
-  return importNotionSource(args, options)
-    .catch(reporter.error)
-    .finally(() => {
-      activity.end();
-    });
-}
-
-export const sourceNodes: GatsbyNode['sourceNodes'] = (args, options: PluginOptions & Options) => {
-  time(args, options, `Fetching data from notion database ${options.databaseId}`)
-};
+export const sourceNodes: GatsbyNode['sourceNodes'] = (args, options: PluginOptions & Options) => 
+  importNotionSource(args, options)
 
 export const onCreateDevServer: GatsbyNode['onCreateDevServer'] = (
   args,
@@ -35,10 +22,19 @@ export const onCreateDevServer: GatsbyNode['onCreateDevServer'] = (
       }
       intervalFlag = true;
 
-      time(args, options, `Refetching data from notion database ${options.databaseId}`).finally(() => {
-        intervalFlag = false;
-      })
+      const activity = reporter.activityTimer(
+        `Refetching data from notion database ${options.databaseId}`,
+      );
+      activity.start();
+
+      importNotionSource(args, options)
+        .catch(reporter.error)
+        .finally(() => {
+          intervalFlag = false;
+          activity.end();
+        });
     };
+
     setInterval(intervalFunc, devServerRefreshInterval);
   }
 };
