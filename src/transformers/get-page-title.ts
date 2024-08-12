27bcc293d634714099187a1c80fe20bf9388c45a
Themion <themion@naver.com>
2024-08-12 16:46:30 +0900

    Fix/remove escape from property (#83)
    * fix: remove html escaping from property
    
    * fix: use row title as plain text
    
    * fix: remove slot
    
    * style: fix lint
    
    * chore: bump version

--- src/transformers/get-page-title.ts
+++ src/transformers/get-page-title.ts
@@ -1,4 +1,3 @@
-import { blockToString } from '~/block-to-string';
 import type { Page } from '~/types';
 
 type TitleProperty = Extract<Page['properties'][string], { type: 'title' }>;
@@ -7,4 +6,4 @@ const getTitleProperty = (properties: Page['properties']) =>
   Object.values(properties).find(({ type }) => type === 'title') as TitleProperty | undefined;
 
 export const getNotionPageTitle = (page: Page) =>
-  blockToString(getTitleProperty(page.properties)?.title ?? []);
+  (getTitleProperty(page.properties)?.title.map(({ plain_text }) => plain_text) ?? []).join('');
